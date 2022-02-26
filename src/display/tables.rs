33856ae12b90594b812cbe25258619cfd680435b
Lukasz Celitan <lukasz.celitan@eu.jll.com>
2022-02-26 23:05:48 +0100

    feat(compare): adjust the fiffrence display field

--- src/display/tables.rs
+++ src/display/tables.rs
@@ -28,31 +28,33 @@ pub fn display_files_table(files: &HashSet<FileInfo>) {
     table.printstd();
 }
 
-fn format_diff(diff: i128) -> String {
+fn format_diff(diff: i128, original_size: &u64) -> String {
+    let percentage = ((diff.abs() as f64 / *original_size as f64) * 100.0).round();
+
     if diff < 0 {
-        return "- ".to_owned()
-            + SizeFormatterBinary::from::<u64>(
+        return format!(
+            "{}% (-{}B)",
+            percentage,
+            SizeFormatterBinary::from::<u64>(
                 u64::try_into(diff.abs().try_into().unwrap()).unwrap(),
-            )
-            .to_string()
-            .as_str()
-            + "B";
+            ),
+        );
     } else {
-        return "+ ".to_owned()
-            + SizeFormatterBinary::from::<u64>(
+        return format!(
+            "{}% (+{}B)",
+            percentage,
+            SizeFormatterBinary::from::<u64>(
                 u64::try_into(diff.abs().try_into().unwrap()).unwrap(),
-            )
-            .to_string()
-            .as_str()
-            + "B";
+            ),
+        );
     }
 }
 
-fn get_correct_cell(diff: &i128) -> Cell {
+fn get_correct_cell(diff: &i128, original_size: &u64) -> Cell {
     if diff < &0 {
-        return Cell::new(&format_diff(*diff)).style_spec("Fgb");
+        return Cell::new(&format_diff(*diff, original_size)).style_spec("Fgb");
     }
-    return Cell::new(&format_diff(*diff)).style_spec("Frb");
+    return Cell::new(&format_diff(*diff, original_size)).style_spec("Frb");
 }
 
 pub fn display_report_table(files: HashSet<FileReport>) {
@@ -71,7 +73,7 @@ pub fn display_report_table(files: HashSet<FileReport>) {
             Cell::new(&file.resolution),
             Cell::new(&file.original_formatted_size).style_spec("Fm"),
             Cell::new(&file.optimized_formatted_size).style_spec("Fmb"),
-            get_correct_cell(&file.size_diff),
+            get_correct_cell(&file.size_diff, &file.original_size),
         ]));
     }
 
--- src/operations/compare.rs
+++ src/operations/compare.rs
@@ -4,6 +4,7 @@ use std::collections::HashSet;
 #[derive(PartialEq, Eq, Hash)]
 pub struct FileReport {
     pub name: String,
+    pub original_size: u64,
     pub original_formatted_size: String,
     pub optimized_formatted_size: String,
     pub resolution: String,
@@ -25,6 +26,7 @@ pub fn compare_files(files: HashSet<FileInfo>, output_dir: String) -> HashSet<Fi
         let diff = compare_file_sizes(&output_file, &found_item);
         let file_report = FileReport {
             name: output_file.name,
+            original_size: found_item.size_bytes,
             original_formatted_size: original_formatted_size,
             optimized_formatted_size: output_file.size_formatted,
             resolution: output_file.resolution,
